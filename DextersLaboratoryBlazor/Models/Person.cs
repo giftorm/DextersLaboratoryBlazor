c58f04a287d0c25d1dc4de07224d22a34fed7a70
Bill Tell <bill.tell@novatrox.se>
2020-02-07 16:21:21 +0100

    initial commit of blazor project. built simple components and has only one page atm

---
+++ DextersLaboratoryBlazor/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DextersLaboratoryBlazor.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+        public string Description { get; set; }
+        public string Story { get; set; }
+    }
+}
